5e1a15cb875aa111530afc2e32d0fea27bfd0e2f
Yanchapanta <marco_broter@hotmail.com>
2025-01-17 11:58:33 -0500

    actualizacion colores, y colores automáticos que muestra

--- borrador/main_colors.js
+++ borrador/main_colors.js
@@ -1,4 +1,5 @@
-
+let variableStaticA="Hello world";
+let variableStaticB="dos";
 
 //LISTA
 let listWord=[
@@ -12,17 +13,17 @@ let listWord=[
     ["8","	Chocolate","	Chocolate","	The cake is dark chocolate","	El pastel es de chocolate oscuro","	Un sabor y color común para los pasteles"],
     ["9","	Coral","	Coral","	The coral reef is colorful","	El arrecife de coral es colorido","	Los arrecifes de coral son conocidos por su gran variedad de colores"],
     ["10","	Crimson","	Carmesí","	The rose is a deep crimson","	La rosa es de un rojo carmesí intenso","	Crimson es un tono de rojo muy intenso y oscuro"],
-    ["11","	DarkGray","Gris oscuro","The clouds are dark gray","	Las nubes son de un gris oscuro","	Un color común para las nubes antes de una tormenta"],
+    ["11","	Dark Gray","Gris oscuro","The clouds are dark gray","	Las nubes son de un gris oscuro","	Un color común para las nubes antes de una tormenta"],
     ["12","	Gold","	Dorado","	The sun is shining gold","	El sol brilla dorado","	Una descripción poética del color del sol"],
     ["13","	Gray","	Gris","	The rock is gray and old","	La roca es gris y vieja","	Un color común para las rocas y las piedras"],
     ["14","	Green","	Verde","	The grass is so green","	La hierba está muy verde","	Un color asociado con la naturaleza y la vida"],
     ["15","	Indigo","	Índigo","	The flower is a bright indigo","	La flor es de un color índigo brillante","	Indigo es un tono de azul oscuro, casi morado"],
     ["16","	Ivory","	Marfil","	The dress is a soft ivory","	El vestido es de un suave color marfil","	Ivory es un color blanco cremoso, a menudo asociado con la elegancia"],
     ["17","	Lavender","	Lavanda","	The field of lavender is purple","	El campo de lavanda es morado","	Lavender es un tono de morado pálido y suave"],
-    ["18","	LightGray","	Gris claro","	The clouds are light gray","	Las nubes son de un gris claro","	Un color común para las nubes en un día nublado"],
+    ["18","	Light Gray","	Gris claro","	The clouds are light gray","	Las nubes son de un gris claro","	Un color común para las nubes en un día nublado"],
     ["19","	Magenta","	Magenta","	The flower is a bright magenta","	La flor es de un color magenta brillante","	Magenta es un tono de rosa muy intenso y vibrante"],
     ["20","	Maroon","	Marrón oscuro","	The carpet is a deep maroon","	La alfombra es de un color burdeos intenso","	Maroon es un tono de rojo oscuro, casi marrón"],
-    ["21","	MistyRose","	Rosa pálido","	The sky is a misty rose","	El cielo es de un rosa pálido","	Misty rose es un tono de rosa muy suave y delicado"],
+    ["21","	Misty Rose","	Rosa pálido","	The sky is a misty rose","	El cielo es de un rosa pálido","	Misty rose es un tono de rosa muy suave y delicado"],
     ["22","	Navy","	Azul marino","	The sailor's uniform is navy blue","	El uniforme de marinero es azul marino","	Navy blue es un tono de azul oscuro, asociado con la marina"],
     ["23","	Olive","	Oliva","	The olive oil is green","	El aceite de oliva es verde","	El aceite de oliva virgen extra tiene un color verde"],
     ["24","	Orange","	Naranja","	The orange is so juicy","	La naranja está muy jugosa","	Un color y sabor asociados con la fruta"],
@@ -37,105 +38,87 @@ let listWord=[
     ["33","	Teal","	Azul verdoso","	The duck is a bright teal","	El pato es de un color azul verdoso brillante","	Teal es un tono de azul verdoso, similar al color del agua"],
     ["34","	Turquoise","	Turquesa","	The water is a bright turquoise","	El agua es de un color turquesa brillante","	Turquoise es un tono de azul verdoso, asociado con el agua"],
     ["35","	White","	Blanco","	The snow is pure white","	La nieve es blanca pura","	Un color asociado con la pureza y el invierno"],
-    ["36","	WhiteSmoke","	Blanco","The smoke is white and wispy","	El humo es blanco y tenue","	Una descripción del humo blanco"],
+    ["36","	White Smoke","	Blanco","The smoke is white and wispy","	El humo es blanco y tenue","	Una descripción del humo blanco"],
     ["37","	Yellow","	Amarillo","	The lemon is bright yellow","	El limón es de un amarillo brillante","	Un color asociado con la fruta y el sol"]    
 ]
+let numAleatorio1=numAleatorio(),
+    speakRamdonNum=listWord[numAleatorio1][0],  
+    speakRamdonWord=listWord[numAleatorio1][1],  
+    speakRamdonWordES=listWord[numAleatorio1][2],  
+    speakRamdon=listWord[numAleatorio1][3],  
+    spanishRamdon=listWord[numAleatorio1][4],
+    numberA=document.getElementById("numberA");   
+    variableStaticA=numAleatorio1;
 
-const teamA_numStart=document.getElementById("teamA_numStart"),
-      teamA_numEnd=document.getElementById("teamA_numEnd");
-let numStart=teamA_numStart.textContent-1,
-    numEnd=teamA_numEnd.textContent-1;//listWord.length/2 →22 
-  
-let teamA_rowRamdon=teamA_numAleatorio(numStart,numEnd),
-    teamA_colNum=listWord[teamA_rowRamdon][0],  
-    teamA_colWordEN=listWord[teamA_rowRamdon][1],  
-    teamA_colWordES=listWord[teamA_rowRamdon][2],  
-    teamA_colPhraseEN=listWord[teamA_rowRamdon][3],  
-    teamA_colPhraseES=listWord[teamA_rowRamdon][4],
-    teamA_num_random=document.getElementById("teamA_num_random");  
-  
 
 //CARGA DEFAULT
-document.addEventListener("DOMContentLoaded",(e)=>{    
-    teamA_phraseToSpeak(); 
-    teamA_num_random.textContent= teamA_colNum;
-    teamA_num_random.setAttribute("href",`#teamA_fila${teamA_colNum}`);
+document.addEventListener("DOMContentLoaded",(e)=>{       
+    textToSpeak(); 
+    numberA.textContent= speakRamdonNum;
+    numberA.setAttribute("href",`#fila${speakRamdonNum}`);
 
 });  
-
-   
-
-function teamB_numAleatorio(inicio,final){
-    //del 23 al 44
+function numAleatorio(){
     let numberSelected = 0;
     for (let i = 0; i < listWord.length; i++) {        
         numberSelected++;
     }
-    return getRandomInt_teamB(listWord.length/2+1,numberSelected) 
-}  
-
+    return getRandomInt(numberSelected) 
+}         
 
 //Función consegir un número aleatorio
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
-
-function  teamA_numAleatorio(numStart,numEnd) {
-    const minCeiled = Math.ceil(numStart);
-    const maxFloored = Math.floor(numEnd);
-    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
-}
-
-
-
-let teamA_inputEN_phrase=document.getElementById("teamA_inputEN_phrase"),
-    teamA_wordEnglish=document.getElementById("teamA_wordEnglish"),
-    teamA_wordSpanish=document.getElementById("teamA_wordSpanish");
-
- 
-teamA_inputEN_phrase.value=teamA_colPhraseEN;
-teamA_wordEnglish.textContent=teamA_colWordEN; 
+//textEscrito de input principal
+let textEscrito=document.getElementById("textEscrito");
+let wordEnglish=document.getElementById("wordEnglish");
+let wordSpanish=document.getElementById("wordSpanish");
+textEscrito.value=speakRamdon;
+wordEnglish.textContent=speakRamdonWord; 
 
 // BOTON REPETIR
-document.getElementById('teamA_btn_repeat').addEventListener("click",()=>{
-    teamA_phraseToSpeak();  
+document.getElementById('btn_play').addEventListener("click",()=>{
+    textToSpeak()    
 });
 //BOTON MUESTRAME
-
-document.getElementById('teamA_btn_showMe').addEventListener("click",()=>{
-   teamA_inputEN_phrase.classList.add("showYes");   
-   teamA_wordEnglish.classList.add("showYes");
-   //color atomático
-   teamA_fila=document.getElementById(`teamA_fila${teamA_rowRamdon}`);
-    //  teamA_fila.style.backgroundColor="red";
-    for (let i = 0; i < listWord.length; i++) {
-        
-        
-        if(i === teamA_rowRamdon){
-           // teamA_fila.style.backgroundColor="red";
-           console.log(listWord[i][1].trim());
-            teamA_num_random.style.backgroundColor=listWord[i][1].trim()
-           
-        }
-        for (let j = 0; j < listWord[0].length; j++) {
-
-        }        
-     }   
+document.getElementById('btn_showMe').addEventListener("click",()=>{
+   textEscrito.classList.add("showYes")   
+   wordEnglish.classList.add("showYes")   
 });
 
 //BOTON ESPAÑOL
-let teamA_inputES_phrase=document.getElementById("teamA_inputES_phrase");
-document.getElementById('teamA_btn_spanish').addEventListener("click",()=>{
-    teamA_inputES_phrase.value=teamA_colPhraseES;
-    teamA_wordSpanish.textContent=teamA_colWordES;
+let toTextSpanish=document.getElementById("toTextSpanish");
+document.getElementById('btn_spanish').addEventListener("click",()=>{
+    toTextSpanish.value=spanishRamdon;
+    wordSpanish.textContent=speakRamdonWordES;
    
 });
 
 //TRASFORMACION DE VOZ
 
 let utterance = new SpeechSynthesisUtterance();/* convierte texto a voz */
-function teamA_phraseToSpeak(){
-    utterance.text =teamA_colPhraseEN;
+
+function textToSpeak(){
+    //utterance.text =listWord[numAleatorio()][2];   
+    utterance.text =speakRamdon;
+    utterance.rate="0.7";
+    utterance.pitch = "1.1";  
+    utterance.name = "Google US English";
+    utterance.voiceURI = "Google US English";
+    utterance.lang = "en-US";
+     
+    window.speechSynthesis.speak(utterance);   
+
+    /** 
+     * Google US English _ en-US
+        Google UK English Female _ en-GB
+        Google UK English Male _ en-GB 
+    */ 
+}
+function textToSpeakword(){
+    //utterance.text =listWord[numAleatorio()][2];   
+    utterance.text =speakRamdonWord;
     utterance.rate="0.7";
     utterance.pitch = "1.1";  
     utterance.name = "Google US English";
@@ -151,79 +134,74 @@ function teamA_phraseToSpeak(){
     */ 
 }
 
-function teamA_wordToSpeak(){
-    utterance.text =teamA_colWordEN;
-    utterance.rate="0.7";
-    utterance.pitch = "1.1";  
-    utterance.name = "Google US English";
-    utterance.voiceURI = "Google US English";
-    utterance.lang = "en-US";
-     
-    window.speechSynthesis.speak(utterance);      
-}
-
- 
-
  /**
-  * LISTA CONTENIDO DE FRASES  team A
+  * LISTA CONTENIDO DE FRASES
   */
-const tblBody_teamA = document.createElement("tbody");
-const teamA_tablas=document.getElementById("teamA_tablas");
+ const tblBody = document.createElement("tbody");
+ const tabla=document.getElementById("tablas");
+
+ // Crea las celdas
 for (let i = 0; i < listWord.length; i++) {
-    const hilera = document.createElement("tr");
-         hilera.setAttribute("id",`teamA_fila${i}`);
- 
-    for (let j = 0; j < listWord[0].length; j++) {
-      const celda = document.createElement("td");
-      const textoCelda = document.createTextNode(listWord[i][j]
-      );
-      celda.appendChild(textoCelda);
-      hilera.appendChild(celda);
-    }
+   // Crea las hileras de la tabla
+   const hilera = document.createElement("tr");
+        hilera.setAttribute("id",`fila${i}`);
+
+   for (let j = 0; j < listWord[0].length; j++) {
+     // Crea un elemento <td> y un nodo de texto, haz que el nodo de
+     // texto sea el contenido de <td>, ubica el elemento <td> al final
+     // de la hilera de la tabla
+     const celda = document.createElement("td");
+     const textoCelda = document.createTextNode(listWord[i][j]
+     );
+     celda.appendChild(textoCelda);
+     hilera.appendChild(celda);
+   }
+   
+   // agrega la hilera al final de la tabla (al final del elemento tblbody)
+   tblBody.appendChild(hilera);
+   
+}
+tabla.appendChild(tblBody);
+
+let fila=" ";
+let ban=1;
+numberA.addEventListener("click",()=>{ 
+
+    console.log("fila",fila)
     
-    tblBody_teamA.appendChild(hilera);
-    
- }
- teamA_tablas.appendChild(tblBody_teamA);
-
-
-//COLOREAR FILAS teamA
-let teamA_fila=" ";
-
-teamA_num_random.addEventListener("click",()=>{ 
-
-    
-    teamA_fila=document.getElementById(`teamA_fila${teamA_rowRamdon}`);
-    //  teamA_fila.style.backgroundColor="red";
+    fila=document.getElementById(`fila${numAleatorio1}`);
+    //  fila.style.backgroundColor="red";
     for (let i = 0; i < listWord.length; i++) {
         
         
-        if(i === teamA_rowRamdon){
-            teamA_fila.style.backgroundColor="red";
-           
+        if(i === numAleatorio1){
+            fila.style.backgroundColor="red";
+            variableStaticA=numAleatorio1;
         }
         for (let j = 0; j < listWord[0].length; j++) {
 
-        }        
+        }
+        
      }
+
     
 });
+console.log(variableStaticA)
+console.log(variableStaticB)
+//BOTON siguiente
+let btn_next=document.getElementById('btn_next');
+let toText = document.getElementById("toText");
 
-
-
-
-//teamA boton siguiente teamA
-let teamA_btn_next=document.getElementById('teamA_btn_next');
-
-teamA_btn_next.addEventListener("click",()=>{
-    teamA_fila=document.getElementById(`teamA_fila${teamA_rowRamdon}`);
+btn_next.addEventListener("click",()=>{
+    fila=document.getElementById(`fila${numAleatorio1}`);
     for (let i = 0; i < listWord.length; i++) {
         
-        if(i !== teamA_rowRamdon){
-            teamA_fila.style.backgroundColor="green";
+        if(i !== numAleatorio1){
+            console.log("direrente",i,numAleatorio1)
+            fila.style.backgroundColor="green";
         }
-        if(i === teamA_rowRamdon){
-            teamA_fila.style.backgroundColor="red";
+        if(i === numAleatorio1){
+            fila.style.backgroundColor="red";
             
         }
         for (let j = 0; j < listWord[0].length; j++) {
@@ -232,35 +210,34 @@ teamA_btn_next.addEventListener("click",()=>{
         
      }
 
-    teamA_fila=document.getElementById(`teamA_fila${teamA_rowRamdon}`);
-    teamA_rowRamdon=teamA_numAleatorio(numStart,numEnd);
-    teamA_colNum=listWord[teamA_rowRamdon][0],  
-    teamA_colWordEN=listWord[teamA_rowRamdon][1],  
-    teamA_colWordES=listWord[teamA_rowRamdon][2],  
-    teamA_colPhraseEN=listWord[teamA_rowRamdon][3],  
-    teamA_colPhraseES=listWord[teamA_rowRamdon][4],
+    fila=document.getElementById(`fila${numAleatorio1}`);
+    numAleatorio1=numAleatorio()
+    speakRamdonNum=listWord[numAleatorio1][0],  
+    speakRamdonWord=listWord[numAleatorio1][1],  
+    speakRamdonWordES=listWord[numAleatorio1][2],  
+    speakRamdon=listWord[numAleatorio1][3],  
+    spanishRamdon=listWord[numAleatorio1][4];      
+    textToSpeak();
+    variableStaticA=numAleatorio1;
+    console.log(variableStaticA)
+    textEscrito.value=speakRamdon;
+    wordEnglish.textContent=speakRamdonWord; 
+    wordSpanish.textContent=speakRamdonWordES;
+    numberA.textContent=  speakRamdonNum;
+    numberA.setAttribute("href",`#fila${speakRamdonNum}`);
+    //fila.style.backgroundColor="red";
+
+    toTextSpanish.value=" ";
+    wordSpanish.textContent=" ";
+    textEscrito.classList.remove("showYes");
+    wordEnglish.classList.remove("showYes"); 
 
     
-    teamA_phraseToSpeak();
-   
-  
-    teamA_inputEN_phrase.value=teamA_colPhraseEN;
-    teamA_wordEnglish.textContent=teamA_colWordEN; 
-    teamA_wordSpanish.textContent=teamA_colWordES;
-    teamA_num_random.textContent=  teamA_colNum;
-    teamA_num_random.setAttribute("href",`#teamA_fila${teamA_colNum}`);
-    //teamA_fila.style.backgroundColor="red";
-
-    teamA_inputES_phrase.value=" ";
-    teamA_wordSpanish.textContent=" ";
-    teamA_inputEN_phrase.classList.remove("showYes");
-    teamA_wordEnglish.classList.remove("showYes");     
+     
 
 });
 
-teamA_wordEnglish.addEventListener("click",()=>{
-    teamA_wordToSpeak();
+wordEnglish.addEventListener("click",()=>{
+    textToSpeakword();
+    console.log("soy español") 
 });
-
-
-
